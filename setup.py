b8b7f58cf19da2e2385c49895cd9293e8b9104f5
Kristian Glass <git@doismellburning.co.uk>
2012-02-11 15:47:33 +0000

    Added a setup.py and jiggled directory structure to be a little more useful

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='libchess',
+      description='Simple chess engine',
+      author='Kristian Glass',
+      author_email='libchess@doismellburning.co.uk',
+      url='https://github.com/doismellburning/libchess',
+      version='0.1',
+      py_modules=['libchess'],
+      )
